d28474f499c7adbb3d86e6ea9fe59958512e94a7
hmz273 <73111873+hmz273@users.noreply.github.com>
2022-11-08 09:42:11 +0100

    rr

--- booking-app/core/controllers/hotel.controller.js
+++ booking-app/core/controllers/hotel.controller.js
@@ -6,10 +6,13 @@ export const createHotel = async (req, res) => {
   const { title, city, street } = result.value;
   try {
     if (!req.files) {
-      return res.status(400),send({
-        error: true,
-        message: 'Please upload at least one image',
-      });
+      return (
+        res.status(400),
+        send({
+          error: true,
+          message: 'Please upload at least one image',
+        })
+      );
     }
 
     if (result.error) {
